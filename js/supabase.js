77736f228c8441dd6a75fa46453b7d48d846b654
Zr-udn <badboyav555@gmail.com>
2026-05-28 12:48:06 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,12 @@
+//const SUPABASE_URL = 'https://gbhglongaehgspeifovp.supabase.co';
+//const SUPABASE_ANON_KEY = 'sb_publishable_yMZYjsVqJ9RchfeI3uO1bQ_0g_s2mUi';
+//const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Supabase Configuration
 const SUPABASE_URL = 'https://gbhglongaehgspeifovp.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_yMZYjsVqJ9RchfeI3uO1bQ_0g_s2mUi';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Create client
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Make it global
+window.supabase = supabaseClient;
